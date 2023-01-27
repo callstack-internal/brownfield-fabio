2c6fc8079e5b184bf9b3d398e05a727f495a6bc4
Fábio Henriques <fabio.henriques@callstack.com>
2023-01-27 12:04:01 +0000

    Setup iOS project

---
+++ ios/BrownfieldFabioApp/BrownfieldFabioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrownfieldFabioAppApp.swift
+//  BrownfieldFabioApp
+//
+//  Created by Fábio Henriques on 27/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BrownfieldFabioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
