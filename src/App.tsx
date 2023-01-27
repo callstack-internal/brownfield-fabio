b46c940d60cd7cc55615f8551b446341f726a523
Fábio Henriques <fabio.henriques@callstack.com>
2023-01-27 17:18:36 +0000

    Setup some React Native related files

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.rootContainer}>
+      <Text>React Native App</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+  },
+});
+
+export default App;
